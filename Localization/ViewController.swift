c046aed9aaac5d103eaf84819686d01078feed8f
Ahmed Kassem <ahmedkassem996@gmail.com>
2020-04-01 00:28:10 +0200

    Initial Commit

---
+++ Localization/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Localization
+//
+//  Created by AHMED on 1/13/1399 AP.
+//  Copyright © 1399 AHMED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
